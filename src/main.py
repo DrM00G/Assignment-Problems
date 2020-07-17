ef9bc30db9f50c19a8b804bcf9c183e52b4f1d8c
Dr. Moo <awesomedigger1021@gmail.com>
2020-07-17 20:45:09 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from Divide_Conquar import divide_and_conquer_sort
+
+print(divide_and_conquer_sort([1,2,4,3,5,8,7,9]))
+
+print(divide_and_conquer_sort([5,8,7,9]))
+
+
+print(divide_and_conquer_sort([100,2,9,8]))
+
+print(divide_and_conquer_sort([1,2,9,8,1,2,5,6,2]))
+
